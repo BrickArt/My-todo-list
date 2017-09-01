7e8f6a9a5c5352b16b1f5b0d60966277c8bcc50b
Artem Rebrik <31184570+BrickArt@users.noreply.github.com>
2017-09-01 21:27:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ var app = express ();
 //===========================================
 // Configurate
 //===========================================
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
